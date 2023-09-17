db1e09e0f22f4ebda77aa3a557b415ff13b4c5f6
Hafsa Hameed <hafsa.hameed457@gmail.com>
2023-09-18 01:27:58 +0500

    gitignore

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import parse from "csv-parse";
+import { parse } from "csv-parse";
 import fs from "fs";
+let file_data = [];
+
+fs.createReadStream("kepler_Data.csv")
+  .pipe(parse({ comment: "#", columns: true }))
+  .on("data", (data) => {
+    file_data.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(file_data);
+  });
