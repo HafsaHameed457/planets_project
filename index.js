326aaf9c7748c3fa41cd12e3c4ea293a303b9b8c
Hafsa Hameed <hafsa.hameed457@gmail.com>
2023-09-18 03:11:51 +0500

    csv file read

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import { parse } from "csv-parse";
 import fs from "fs";
-let file_data = [];
+let habitatable_Planets = [];
 
+function isHabitatable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 fs.createReadStream("kepler_Data.csv")
   .pipe(parse({ comment: "#", columns: true }))
   .on("data", (data) => {
-    file_data.push(data);
+    if (isHabitatable(data)) {
+      habitatable_Planets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(file_data);
+    console.log(habitatable_Planets.map((planet) => planet.kepoi_name));
   });
